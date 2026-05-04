9460fa7b027fc186bbbb2a8f0652826285e275a3
Jules Crevola <julescrevola@gmail.com>
2026-05-04 16:11:40 +0200

    fix: fixed bash script usage of domain env variable and ip_address retrieval

--- cli-aliases.sh
+++ cli-aliases.sh
@@ -45,7 +45,7 @@ deploy(){
         echo "Applying Kubernetes deployment..."
         kubectl apply -f deployment.yaml
 
-        IP_ADDRESS=$(kubectl get svc linkedin-games-ranking -o jsonpath='{.status.loadBalancer.ingress[0].externalIP}')
+        IP_ADDRESS=$(kubectl get svc linkedin-games-ranking -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
         if [[ -n $DOMAIN ]]; then
             echo "Deployment complete. Access the application at https://$DOMAIN"
         else
@@ -60,7 +60,7 @@ deploy(){
         echo "Updating Kubernetes deployment with new image digest..."
         kubectl set image deployment/linkedin-games-ranking linkedin-games-ranking=julescrevola/linkedin-games@$IMAGE_DIGEST
 
-        IP_ADDRESS=$(kubectl get svc linkedin-games-ranking -o jsonpath='{.status.loadBalancer.ingress[0].externalIP}')
+        IP_ADDRESS=$(kubectl get svc linkedin-games-ranking -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
         if [[ -n $DOMAIN ]]; then
             echo "Deployment updated. Access the application at https://$DOMAIN"
         else
